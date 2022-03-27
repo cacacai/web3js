3c3f857987f183565b606ce2758a9cd7d28a42c9
Cai <jiashengcai97@gmail.com>
2022-03-27 17:18:54 +0000

    调用合约中的转账事件,查看转账后的余额

---
+++ example/5_write_contract.js
@@ -0,0 +1,60 @@
+var Tx = require('ethereumjs-tx').Transaction
+const Web3 = require('web3')
+const web3 = new Web3('https://goerli.infura.io/v3/5d368be9da12461ba679dd39ba212f33')
+/**
+ * account1 作为合约的创建者，所以拥有合约中的所有的代币
+ * account2 将会被作为接收代码的地址
+ */
+const account1 = '0x172a6c2f64700165fAF202f269d3Ad873D0c7E4b' // Your account address 1
+const account2 = '0x427E5D0A21e129883527DCCaeA8d1327d153884b' // Your account address 2
+
+const privateKey1 = Buffer.from('', 'hex')
+
+/**
+ * 这个合约中，定义代币，所以在后续中可以把币转到不同的账户中
+ */
+const contractAddress = '0xb37443C506a95a0279305FeBDF0E8E78458D3ABa'
+const contractABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"standard","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]
+
+const contract = new web3.eth.Contract(contractABI, contractAddress)
+
+//Transfer some tokens
+/**
+ *  contractAddress
+ */
+web3.eth.getTransactionCount(account1, (err, txCount) => {
+
+  const txObject = {
+    nonce:    web3.utils.toHex(txCount),
+    gasLimit: web3.utils.toHex(800000), // Raise the gas limit to a much higher amount
+    gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+    to: contractAddress,
+    data: contract.methods.transfer(account2, 1000).encodeABI()
+    //调用合约中的方法，向account2发送1000个代币
+  }
+
+  const tx = new Tx(txObject,{chain: 'goerli'})
+  tx.sign(privateKey1)
+
+  const serializedTx = tx.serialize()
+  const raw = '0x' + serializedTx.toString('hex')
+
+  web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+    console.log('err:', err, 'txHash:', txHash)
+    // Use this txHash to find the contract on Etherscan!
+  })
+})
+
+/**
+ * 经过转账之后，检查账户的中的代币余额
+ * account1 减少 account2 增加
+ */
+// Check Token balance for account1
+contract.methods.balanceOf(account1).call((err, balance) => {
+  console.log({ err, balance })
+})
+
+// Check Token balance for account2
+contract.methods.balanceOf(account2).call((err, balance) => {
+  console.log({ err, balance })
+})
